68c2b63e672e119a8f627cdea20e0ab4c4a04d11
Methyl <73468685+ScriptingForLife@users.noreply.github.com>
2021-06-08 09:57:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-require("./app/routes/customer.routes.js")(app);
+require("./app/routes/client.routes.js")(app);
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
